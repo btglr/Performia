9f57b57017a8c6312d3d73a2d9d13f573efff55b
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-10-13 21:28:10 +0200

    Switch-case basique sur le code des requêtes reçues

--- serveur_http/src/serveur/RequestHandler.java
+++ serveur_http/src/serveur/RequestHandler.java
@@ -3,10 +3,13 @@ package serveur;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import utils.QueryUtils;
+import utils.RequestCode;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Map;
+
+import static utils.RequestCode.*;
 
 public class RequestHandler implements HttpHandler {
     public void handle(HttpExchange t) {
@@ -15,7 +18,29 @@ public class RequestHandler implements HttpHandler {
         String query = t.getAttribute("query").toString();
         Map<String, String> parameters = (Map<String, String>) t.getAttribute("parameters");
 
-        System.out.println(timestamp + " - Received query with data " + query);
+        System.out.println(timestamp + " - Received query with parameters " + query);
+
+        // Vérifications de base
+        if (parameters != null) {
+            if (parameters.containsKey("code")) {
+                RequestCode code = getRequest(Integer.parseInt(parameters.get("code")));
+
+                // Le code permet d'identifier la requête
+                switch (code) {
+                    case CHOOSE_CHALLENGE:
+                        System.out.println("User has chosen a challenge");
+                        break;
+
+                    case PLAY_TURN:
+                        System.out.println("User had made a move");
+                        break;
+
+                    case UNKNOWN:
+                    default:
+                        System.out.println("Received an unknown request");
+                }
+            }
+        }
 
         String r = "<h1>Welcome</h1>";
         QueryUtils.sendHTTPResponse(t, r);
