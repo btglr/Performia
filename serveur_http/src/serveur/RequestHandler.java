515dd09bab6c8c8bdf2df3ea0cb282588c5faf97
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-10-13 20:55:40 +0200

    Récupération des paramètres parsés par le filtre

--- serveur_http/src/serveur/RequestHandler.java
+++ serveur_http/src/serveur/RequestHandler.java
@@ -4,10 +4,20 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import utils.QueryUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+
 public class RequestHandler implements HttpHandler {
     public void handle(HttpExchange t) {
-        String r = "<h1>Welcome</h1>";
+        String timestamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
 
+        String query = t.getAttribute("query").toString();
+        Map<String, String> parameters = (Map<String, String>) t.getAttribute("parameters");
+
+        System.out.println(timestamp + " - Received query with data " + query);
+
+        String r = "<h1>Welcome</h1>";
         QueryUtils.sendHTTPResponse(t, r);
     }
 }
