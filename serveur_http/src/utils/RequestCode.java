d86e75f4f3dc532884cae75a7b19f024b9226d7c
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-10-13 21:44:39 +0200

    Ajout de plusieurs codes de requêtes/réponses

--- serveur_http/src/utils/RequestCode.java
+++ serveur_http/src/utils/RequestCode.java
@@ -3,10 +3,15 @@ package utils;
 public enum RequestCode {
     UNKNOWN(-1, "Unknown request"),
     CHOOSE_CHALLENGE(2, "Request sent by the web interface when choosing a challenge"),
-    PLAY_TURN(4, "Request sent by the web interface when the user has made a move")
+    INITIAL_GAME_STATE(3, "Answer sent by the HTTP server with the initial state of the game"),
+    PLAY_TURN(4, "Request sent by the web interface when the user has made a move"),
+    ACTION_OK(5, "Answer sent by the HTTP server if the action was successful"),
+    ACTION_NOT_OK(6, "Answer sent by the HTTP server if the action was not successful"),
+    GET_GAME_STATE(7, "Request sent by the web interface regularly to get the game state"),
+    GAME_STATE(8, "Answer sent by the HTTP server with the game state")
     ;
 
-    public final int code;
+    private final int code;
     private final String description;
 
     RequestCode(int code, String description) {
