520f9a941f189d376e85741e017609003275b050
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-10-27 10:23:42 +0100

    Ajout d'un constructeur par défaut, setter pour le code de requête

--- src/requete/Message.java
+++ src/requete/Message.java
@@ -16,25 +16,32 @@ import org.json.JSONObject;
  */
 public class Message {
     private int code;
-    private JSONObject data = new JSONObject();
+    private JSONObject data;
     
-    public Message(int code, JSONObject json){
+    public Message(int code, JSONObject json) {
         this.code = code;
         this.data = json;
     }
     
-    public Message(int code)
-    {
-        this.code = code;
+    public Message(int code) {
+        this(code, new JSONObject());
+    }
+
+    public Message() {
+        this(-1);
     }
       
-    public Message(int code, String data){
+    public Message(int code, String data) {
         this.code = code;
         try {
             this.data = new JSONObject(data);
         } catch (JSONException ex) {
             Logger.getLogger(Message.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public void setCode(int code) {
+        this.code = code;
     }
 
     public int getCode() {
@@ -46,7 +53,9 @@ public class Message {
     }
 
     public void addData(String key, Object data) {
-        this.data.put(key, data);
+        if (this.data != null) {
+            this.data.put(key, data);
+        }
     }
 
     @Override
@@ -65,7 +74,7 @@ public class Message {
         return details;
     }
     
-    public JSONObject toJSON(){
+    public JSONObject toJSON() {
         JSONObject json = new JSONObject();
         try {
             json.put("code",this.code);
@@ -80,7 +89,7 @@ public class Message {
         return json;
     }
     
-    public static Message fromJSON(JSONObject json){
+    public static Message fromJSON(JSONObject json) {
         Message req = null;
         try {
             req =  new Message(json.getInt("code"),new JSONObject(json.getString("data")));
