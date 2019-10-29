b3ce0c28e8d8fee3af1bb19247550c2a354d645a
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-10-29 15:00:08 +0100

    Ajout de nouveaux codes de réponse/requête

--- src/utils/MessageCode.java
+++ src/utils/MessageCode.java
@@ -1,30 +1,37 @@
 package utils;
 
+import static utils.MessageType.*;
+import static utils.ProtocolType.*;
+
 /**
  * Specifications of the different request codes/response codes
  * Response codes : >= 500 ; < 1000
  * Error codes : >= 1000
  */
 public enum MessageCode {
-    UNKNOWN(MessageType.REQUEST, ProtocolType.BOTH, -1, "Unknown request"),
+    UNKNOWN(REQUEST, BOTH, -1, "Unknown request"),
 
     // Requests
-    CONNECTION(MessageType.REQUEST, ProtocolType.BOTH, 1, "Request sent by the web interface or the TCP socket when a user/AI tries to connect"),
-    CHOOSE_CHALLENGE(MessageType.REQUEST, ProtocolType.HTTP, 2, "Request sent by the web interface when choosing a challenge"),
-    PLAY_TURN(MessageType.REQUEST, ProtocolType.BOTH, 3, "Request sent by the web interface or the TCP socket when the user/AI has made a move"),
-    GET_GAME_STATE(MessageType.REQUEST, ProtocolType.HTTP, 4, "Request sent by the web interface regularly to get the game state"),
+    CONNECTION(REQUEST, BOTH, 1, "Request sent by the web interface or the TCP socket when a user/AI tries to connect"),
+    CHOOSE_CHALLENGE(REQUEST, HTTP, 2, "Request sent by the web interface when choosing a challenge"),
+    PLAY_TURN(REQUEST, BOTH, 3, "Request sent by the web interface or the TCP socket when the user/AI has made a move"),
+    GET_CHALLENGE_STATE(REQUEST, HTTP, 4, "Request sent by the web interface regularly to get the challenge state"),
+    WAIT_CHALLENGE_START(REQUEST, BOTH, 5, "Request sent by the web interface/AI when waiting for the challenge to start under certain conditions"),
 
     // Responses
-    INITIAL_GAME_STATE(MessageType.RESPONSE, ProtocolType.HTTP, 500, "Answer sent by the HTTP server with the initial state of the game"),
-    ACTION_OK(MessageType.RESPONSE, ProtocolType.HTTP, 501, "Answer sent by the HTTP server if the action was successful"),
-    GAME_STATE(MessageType.RESPONSE, ProtocolType.HTTP, 502, "Answer sent by the HTTP server with the game state"),
-    CONNECTION_OK(MessageType.RESPONSE, ProtocolType.BOTH, 503, "Answer sent by the HTTP/TCP server when the user/AI successfully connected"),
+    INITIAL_CHALLENGE_STATE(RESPONSE, HTTP, 500, "Answer sent by the HTTP server with the initial state of the challenge"),
+    ACTION_OK(RESPONSE, HTTP, 501, "Answer sent by the HTTP server if the action was successful"),
+    CHALLENGE_STATE(RESPONSE, HTTP, 502, "Answer sent by the HTTP server with the challenge state"),
+    CONNECTION_OK(RESPONSE, BOTH, 503, "Answer sent by the HTTP/TCP server when the user/AI successfully connected"),
+    CHALLENGE_CAN_START(RESPONSE, BOTH, 504, "Answer sent by the HTTP/TCP server as a response to the WAIT_CHALLENGE_START request when the challenge can start"),
+    CHALLENGE_CANNOT_START(RESPONSE, BOTH, 505, "Answer sent by the HTTP/TCP server as a response to the WAIT_CHALLENGE_START when the challenge cannot start"),
+    ROOM_NOT_FULL(RESPONSE, BOTH, 506, "Answer sent by the HTTP/TCP server when a challenge has been chosen but the room is not full"),
 
     // Error responses
-    ACTION_NOT_OK(MessageType.RESPONSE, ProtocolType.BOTH, 1000, "Answer sent by the HTTP server if the action was not successful"),
-    CONNECTION_ERROR(MessageType.RESPONSE, ProtocolType.BOTH, 1001, "Answer sent by the HTTP/TCP server if the connection was not successful"),
-    WRONG_CHALLENGE(MessageType.RESPONSE, ProtocolType.BOTH, 1002, "Answer sent by the HTTP/TCP server if the chose challenge doesn't exist"),
-    GAME_DOESNT_EXIST(MessageType.RESPONSE, ProtocolType.HTTP, 1003, "Answer sent by the HTTP server if the user isn't playing a game")
+    ACTION_NOT_OK(RESPONSE, BOTH, 1000, "Answer sent by the HTTP server if the action was not successful"),
+    CONNECTION_ERROR(RESPONSE, BOTH, 1001, "Answer sent by the HTTP/TCP server if the connection was not successful"),
+    WRONG_CHALLENGE(RESPONSE, BOTH, 1002, "Answer sent by the HTTP/TCP server if the chose challenge doesn't exist"),
+    USER_NOT_PLAYING(RESPONSE, HTTP, 1003, "Answer sent by the HTTP server if the user isn't playing a challenge")
     ;
 
     private final MessageType messageType;
