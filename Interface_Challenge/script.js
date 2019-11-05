bda30a14072d0a1034a8bd9018ea9457ce7ad64c
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-11-05 10:38:51 +0100

    Suppression dossier inutilisé

--- Interface_Challenge/script.js
+++
@@ -1,9 +0,0 @@
-function change_color(i,j){
-    document.getElementById(j+'-'+i).style.backgroundColor="red";    
-}
-
-function choose_col(col){
-    console.log("column select : ",col);
-    //A faire :
-    //sendToJava(col) 
-}
