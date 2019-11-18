9e08fbe6613ee8c361760bf90dfa8aecfa2eea74
namc51 <55434585+namc51@users.noreply.github.com>
2019-11-18 21:20:26 +0100

    Revert "Merge branch 'ChallengeV1'"
    This reverts commit ee8a2548f674100f83a0b2f4acdba0ded5c42c88, reversing
    changes made to 4d669f39e375640b0734379886bc9bdce4a6f2fd.

---
+++ performia_php/challenges/connect4/connect4.php
@@ -0,0 +1,120 @@
+<?php
+
+session_start();
+
+function getColor($col, $p1, $p2) {
+	if ($col === $p1) {
+		return "yellow";
+	}
+	else if ($col === $p2) {
+		return "red";
+	}
+	else {
+		return "white";
+	}
+}
+
+// Connection error
+$code = 1001;
+$id_player = -1;
+
+$php = "<p> Impossible de créer le challenge :</p>
+		<p>     -> serveur http injoignable ?</p>
+		<p>     -> donnees recues invalides ?</p>";
+
+if (isset($_GET["url"])) {
+	$url = $_GET["url"] . "?code=4&id_utilisateur=" . $_SESSION["id"];
+
+	$handle = curl_init($url);
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+
+	$response = curl_exec($handle);
+
+	if ($response) {
+		$decoded = json_decode($response, true);
+
+		if (array_key_exists("data", $decoded)) {
+			if (array_key_exists("grille", $decoded["data"])) {
+				if (array_key_exists("fini", $decoded["data"]) && $decoded["data"]["fini"] === true) {
+				    $code = 507;
+				    $id_player = $decoded["data"]["id_player"];
+				}
+
+				else {
+				    $code = 502;
+                }
+
+				$grille = $decoded["data"]["grille"];
+				$players = $decoded["data"]["players"];
+				$p1 = $players[0]["id"];
+				$p2 = $players[1]["id"];
+
+				$php = "<div style='display:flex; flex-direction:column;height:80%';justify-content:center'>";
+				for ($i = 0; $i < 6; $i++) {
+					for ($j = 0; $j < 7; $j++) {
+						if ($i == 0) {
+							if ($j == 0) {
+								$php .= "<div style='display:flex; flex-direction:column; justify-content:center;'>";
+								$php .= "<div style='background-color:blue; display:flex; flex-direction:row; justify-content:space-between; width:80%;height:75px;border-top:10px solid;border-left:10px solid;border-right:10px solid;margin:auto;max-width:550px'>";
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+							}
+							elseif ($j == 6) {
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+								$php .= "</div></div>";
+							}
+							else {
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+							}
+
+						}
+						elseif ($i == 5) {
+
+							if ($j == 0) {
+								$php .= "<div style='display:flex; flex-direction:column; justify-content:center;'>";
+								$php .= "<div style='background-color:blue; display:flex; flex-direction:row; justify-content:space-between; width:80%;height:75px;border-left:10px solid;border-right:10px solid;border-bottom:10px solid;;margin:auto;max-width:550px'>";
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+							}
+							elseif ($j == 6) {
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+								$php .= "</div></div>";
+							}
+							else {
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+							}
+
+
+						}
+						else {
+
+							if ($j == 0) {
+								$php .= "<div style='display:flex; flex-direction:column; justify-content:center;'>";
+								$php .= "<div style='background-color:blue; display:flex; flex-direction:row; justify-content:space-between; width:80%;height:75px;border-left:10px solid;border-right:10px solid;;margin:auto;max-width:550px'>";
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+							}
+							elseif ($j == 6) {
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+								$php .= "</div></div>";
+							}
+							else {
+								$php .= "<div onclick='choose_col(".$j.")' id=".$j."-".$i." style='background-color:".getColor($grille[$j + $i * 7], $p1, $p2).";display:flex;flex-direction:column; justify-content:center; border:10px solid DarkBlue ;border-radius:100px; width:50px;height:50px;margin-top:auto;margin-bottom:auto'>  </div>";
+							}
+
+						}
+					}
+				}
+				$php .= "</div><br></body></html>";
+			}
+		}
+	}
+}
+
+$result = array(
+    "code" => $code,
+    "php" => $php
+);
+
+if ($id_player !== -1) {
+    $result["id_player"] = $id_player;
+}
+
+echo json_encode($result);
