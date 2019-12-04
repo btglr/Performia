288427ea72b75f5f77a907a7c3bafc231bb24347
namc51 <55434585+namc51@users.noreply.github.com>
2019-12-04 12:08:18 +0100

    Fix: Correction fail merge

--- src/requete/MessageManager.java
+++ src/requete/MessageManager.java
@@ -284,11 +284,9 @@ public class MessageManager implements Runnable {
 		Salle s = findAvailableRoom();
 
 		if (s == null) {
-<<<<<<< HEAD
-			s = new Salle(new Connect4(p));
-=======
+			//s = new Salle(new Connect4(p));
+
 			s = new Salle(new Reflex(p));
->>>>>>> ChallengeV1
 			rooms.add(s);
 		}
 
