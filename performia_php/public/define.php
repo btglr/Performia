d9868de41d88ab7b63cca36ea5d667adc4303772
JVpltr <37187270+JVpltr@users.noreply.github.com>
2019-12-07 10:28:08 +0100

    Add files via upload

--- performia_php/public/define.php
+++ performia_php/public/define.php
@@ -4,18 +4,21 @@ if (!defined("ERROR_")) {
     define('ERROR_', array(
         'Please fill all fields',
         'Wrong username/password combination',
-        'Sorry there is no challenge here for the moment'
+        'Sorry there is no challenge here for the moment',
+        'Password verification doesn\'t match your password',
+        'Date isn\'t valid',
+        'Username is already used'
     ));
 }
 
 if (!defined("HTTP_SERVER_URL")) {
-    define("HTTP_SERVER_URL", "http://http_server:25000");
-}
-
-if (!defined("HTTP_SERVER_AJAX_URL")) {
-	define("HTTP_SERVER_AJAX_URL", "http://localhost:25000");
+    define("HTTP_SERVER_URL", "http://ec2-3-87-99-176.compute-1.amazonaws.com:25000");
 }
 
 if (!defined("REQUEST_HANDLER")) {
 	define("REQUEST_HANDLER", "request");
+}
+
+if (!defined("HTTP_REQUEST_URL")) {
+    define("HTTP_REQUEST_URL", implode("/", array(HTTP_SERVER_URL, REQUEST_HANDLER)));
 }
