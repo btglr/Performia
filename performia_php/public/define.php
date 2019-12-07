bc3668ffc9910e668ce5c4039ae6322db9640e72
JVpltr <37187270+jvpltr@users.noreply.github.com>
2019-12-07 10:28:08 +0100

    Changement CSS pour l'inscription + messages d'erreurs

--- performia_php/public/define.php
+++ performia_php/public/define.php
@@ -4,7 +4,10 @@ if (!defined("ERROR_")) {
     define('ERROR_', array(
         'Please fill all fields',
         'Wrong username/password combination',
-        'Sorry there is no challenge here for the moment'
+        'Sorry there is no challenge here for the moment',
+        'Password verification doesn\'t match your password',
+        'Date isn\'t valid',
+        'Username is already used'
     ));
 }
 
