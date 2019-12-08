eec46d0fedd6d9106e5666f59662870e65e747f5
axelanglet <langlet.axel@hotmail.fr>
2019-12-08 22:58:57 +0100

    add Ai for connect4 in c#

---
+++ AiCSharp/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AiCSharp
+{
+    class Program
+    {
+        static int randomlyChoose(int[] grille)
+        {
+            int res;
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+
+            res = rand.Next(7);
+            while(grille[res] != 0)
+            {
+                res = rand.Next(7);
+                Console.Out.WriteLine("Res = " + res);
+            }
+
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            JObject config = JObject.Parse(File.ReadAllText("config/ia.json"));
+            TCPClient client = new TCPClient();
+            int choice;
+            int[] grid;
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            bool onGoingChallenge = true;
+            int challengeId = 1;
+
+            JObject info;
+            JObject result;
+            client.Connect((String)config.GetValue("login"), (String)config.GetValue("password"));
+
+            JObject initialGameState = client.ChooseChallenge(challengeId);
+            info = JObject.Parse((String)initialGameState.GetValue("data"));
+
+            while (onGoingChallenge)
+            {
+                JObject res = new JObject();
+                onGoingChallenge = !(bool)info.GetValue("fini");
+
+                JArray gridArray = JArray.Parse((String)info.GetValue("grille"));
+                grid = new int[gridArray.Count];
+
+                for (int i = 0; i < gridArray.Count; ++i)
+                {
+                    grid[i] = (int)gridArray[i];
+                }
+
+                if (client.GetUserID() == (int)info.GetValue("id_player") && onGoingChallenge)
+                {
+                    choice = randomlyChoose(grid);
+                    res.Add("id_utilisateur", client.GetUserID());
+                    res.Add("colonne", choice);
+
+                    info = client.PlayTurn(res);
+                }
+                else
+                {
+                    info = JObject.Parse((String)client.GetChallengeState().GetValue("data"));
+                }
+
+                Thread.Sleep(rand.Next(1000, 5000));
+            }
+            client.CloseSocket();
+        }
+    }
+}
---
+++ AiCSharp/TCPClient.cs
@@ -0,0 +1,157 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiCSharp
+{
+
+    class TCPClient
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private int userID;
+
+        public TCPClient()
+        {
+            JObject config = JObject.Parse(File.ReadAllText("config/config.json"));
+            client = new TcpClient("localhost",(int)config["port_tcp"]);
+            stream = client.GetStream();
+        }
+
+        static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public void Connect(String login, String password)
+        {
+            JObject jo = new JObject();
+            jo.Add("login", login);
+            jo.Add("password", Hash(password));
+
+            JObject message = new JObject();
+            message.Add("code", 1);
+            message.Add("data", jo.ToString());
+            SendData(message);
+
+            JObject res = RetrieveData();
+            if((int)res.GetValue("code") != 503 )
+            {
+                Console.Error.WriteLine("Connection failed");
+                Environment.Exit(-1);
+            }
+            userID = (int)JObject.Parse((String)res.GetValue("data")).GetValue("id_utilisateur");
+        }
+
+        private void SendData(JObject mes)
+        {
+            String message = mes.ToString();
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public JObject RetrieveData()
+        {
+            byte[] data = new Byte[256];
+
+            String responseData = String.Empty;
+
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            return JObject.Parse(responseData);
+        }
+
+        public JObject ChooseChallenge(int numeroChallenge)
+        {
+            JObject jo = new JObject();
+            jo.Add("id_utilisateur", this.userID);
+            jo.Add("numero_challenge", numeroChallenge);
+
+            JObject message = new JObject();
+            message.Add("code", 2);
+            message.Add("data", jo.ToString());
+            SendData(message);
+
+            JObject res = RetrieveData();
+            if ((int)res.GetValue("code") != 500)
+            {
+                Console.Error.WriteLine("Choosing a challenge failed");
+                Environment.Exit(-1);
+            }
+            return JObject.Parse((String)res.GetValue("data"));
+        }
+
+        public JObject GetChallengeState()
+        {
+            JObject jo = new JObject();
+            jo.Add("id_utilisateur", this.userID);
+
+            JObject message = new JObject();
+            message.Add("code", 2);
+            message.Add("data", jo.ToString());
+            SendData(message);
+
+            JObject res = RetrieveData();
+            if ((int)res.GetValue("code") != 502)
+            {
+                Console.Error.WriteLine("Couldn't retrieve the challenge state");
+                Environment.Exit(-1);
+            }
+            return JObject.Parse((String)res.GetValue("data"));
+        }
+
+        public void SendTurn(JObject action)
+        {
+            JObject message = new JObject();
+            message.Add("code", 2);
+            message.Add("data", action.ToString());
+            SendData(message);
+        }
+
+        public JObject ReceiveTurn()
+        {
+            JObject res = RetrieveData();
+            if ((int)res.GetValue("code") != 501)
+            {
+                Console.Error.WriteLine("Incorrect move");
+                Environment.Exit(-1);
+            }
+            return JObject.Parse((String)res.GetValue("data"));
+        }
+
+        public JObject PlayTurn(JObject action)
+        {
+            SendTurn(action);
+
+            return ReceiveTurn();
+        }
+
+        public int GetUserID()
+        {
+            return userID;
+        }
+
+        public void CloseSocket()
+        {
+            stream.Close();
+            client.Close();
+        }
+    }
+}
