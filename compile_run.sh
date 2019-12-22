ad27380a781daaa568bef41f279a429a639682d5
Bastien Tagliaro <bastien.tagliaro@gmail.com>
2019-12-22 19:20:36 +0100

    Ajout création dossier de logs

--- compile_run.sh
+++ compile_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p out
+mkdir -p out logs
 javac -d out src/**/*.java -cp "src/libs/json.jar:src/libs/junit.jar:src/libs/mysql-connector.jar"
 javac -d out src/**/**/*.java -cp "out:src/libs/json.jar:src/libs/junit.jar:src/libs/mysql-connector.jar"
 javac -d out src/Performia.java -cp "src/libs/json.jar:src/libs/junit.jar:src/libs/mysql-connector.jar:out"
